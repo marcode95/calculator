38bc5e87710d77ecb1142e725753256458ce3a8d
Marco <marcoerhardt95@gmail.com>
2021-07-08 12:01:32 +0200

    Add test files for button and display components

---
+++ src/__tests__/Button.test.js
@@ -0,0 +1,30 @@
+import renderer from 'react-test-renderer';
+import { render, screen } from '@testing-library/react';
+import Button from '../components/Button';
+import '@testing-library/jest-dom/extend-expect';
+
+describe('checks if Button renders', () => {
+  const clickHandler = () => {
+    '';
+  };
+
+  test('should render', () => {
+    const tree = renderer
+      .create(<Button buttonName=""
+      wide={false}
+      color={false}
+      clickHandler={clickHandler} />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('should display right value', () => {
+    render(<Button buttonName="+"
+    wide={true}
+    color={false} 
+    clickHandler={clickHandler} />);
+    const result = screen.getByText('+');
+
+    expect(result).toBeInTheDocument();
+  });
+});
